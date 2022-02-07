488f488f0c37106aedeee4b51e9559b598645061
brianko <kobj11@gmail.com>
2022-02-07 15:06:07 -0500

    commit tree 2533036d5d21410d45b5cf23c70de6f2cbaaae54

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
+  static capitalize(string) {
+return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^-' A-Za-z0-9]/g, "")
+  
+  }
+  static titleize(string) {
+    if(!string) {
+      return '';
+    }
+    let arr = [];
+    let exludeWords = ['the','a','an','but','of','and','for','at','by','from']
+    arr = string.split(' ');
+    return arr.map((word, index) =>
+    {
+       return exludeWords.includes(word) && index !=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+  
+    }).join(' ');
+  
+  }
   //add static methods here
 }
